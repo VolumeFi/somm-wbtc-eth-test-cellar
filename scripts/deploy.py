4aee4b7784b4ed1a278b3a501db452ead7c50c5b
webelf101 <m63974529@gmail.com>
2022-01-05 01:16:07 +0800

    fix cellar name

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,7 +2,7 @@ from brownie import CellarPoolShare, accounts
 
 def main():
     acct = accounts.load("deployer_account")
-    name = "Test SOMM WETC-ETH"
+    name = "Test SOMM WBTC-ETH"
     symbol = "TESTSOMMWBTCETH"
     token0 = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
     token1 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
